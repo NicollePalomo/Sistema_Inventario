5a6e2344635b09d3a1cc5f4752ab2bace0b30759
Nicolle Palomo R <nicollepalomo68@gmail.com>
2021-08-15 19:27:26 -0500

    se agrega actualiza bd

--- backend/sistema_inventario.sql
+++ backend/sistema_inventario.sql
@@ -1,66 +1,20 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Servidor: localhost
--- Tiempo de generación: 13-04-2021 a las 18:43:36
--- Versión del servidor: 10.4.18-MariaDB
--- Versión de PHP: 8.0.3
+CREATE DATABASE sistema_inventario ;
+USE sistema_inventario ;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `empleados`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `empleados`
---
-
-CREATE TABLE `empleados` (
-  `id` int(11) NOT NULL,
-  `nombre` varchar(255) NOT NULL,
-  `correo` varchar(255) NOT NULL
+CREATE TABLE equipos (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  nombre varchar(255) NOT NULL,
+  periferico varchar(255) NOT NULL,
+  fabricante varchar(255),
+  seriall varchar(255),
+  modelo varchar(255) NOT NULL,
+  procesador varchar(255),
+  ram varchar(255),
+  so varchar(255),
+  area varchar(255) NOT NULL,
+  servicio varchar(255),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Volcado de datos para la tabla `empleados`
---
-
-INSERT INTO `empleados` (`id`, `nombre`, `correo`) VALUES
-(65, 'Oscar', 'oscaruh@develoteca.com');
-
---
--- Índices para tablas volcadas
---
-
---
--- Indices de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de las tablas volcadas
---
-
---
--- AUTO_INCREMENT de la tabla `empleados`
---
-ALTER TABLE `empleados`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ 
+INSERT INTO equipos VALUES
+(1,'confort kyboard 100','teclado','Microsoft','A123','Copmpacto','NA','NA','NA','Farmacia','NA');
